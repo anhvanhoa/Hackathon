1744aacedd3df74eba3ef9812beeba04dcc092e6
AnhVanHoa <anhhoccode@gmail.com>
2023-03-08 05:27:56 +0700

    push code

---
+++ js/app.js
@@ -0,0 +1,14 @@
+let btnMenu = document.querySelector('.header-menu i');
+let navbar = document.querySelector('.header-navbar');
+let closeBtn = document.querySelector('.close i');
+console.log(btnMenu);
+
+btnMenu.addEventListener('click', function() {
+     navbar.classList.toggle('active')
+})
+closeBtn.addEventListener('click', function() {
+     navbar.classList.remove('active');
+})
+window.onscroll = function() {
+     navbar.classList.remove('active');
+}
