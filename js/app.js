1d529e9742fd805d510781610752d9708ad875ab
AnhVanHoa <anhhoccode@gmail.com>
2023-03-10 18:39:06 +0700

    fix scroll row

--- js/app.js
+++ js/app.js
@@ -1,14 +1,13 @@
 let btnMenu = document.querySelector('.header-menu i');
 let navbar = document.querySelector('.header-navbar');
 let closeBtn = document.querySelector('.close i');
-console.log(btnMenu);
 
-btnMenu.addEventListener('click', function() {
-     navbar.classList.toggle('active')
-})
-closeBtn.addEventListener('click', function() {
-     navbar.classList.remove('active');
-})
-window.onscroll = function() {
-     navbar.classList.remove('active');
-}
+btnMenu.addEventListener('click', function () {
+    navbar.classList.toggle('active');
+});
+closeBtn.addEventListener('click', function () {
+    navbar.classList.remove('active');
+});
+window.onscroll = function () {
+    navbar.classList.remove('active');
+};
