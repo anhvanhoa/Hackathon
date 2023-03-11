42020a6dd7d378ab92a5eb8f3c54594845346762
AnhVanHoa <anhhoccode@gmail.com>
2023-03-12 04:20:48 +0700

    fix loi

--- js/app.js
+++ js/app.js
@@ -1,6 +1,10 @@
 let btnMenu = document.querySelector('.header-menu i');
 let navbar = document.querySelector('.header-navbar');
 let closeBtn = document.querySelector('.close i');
+let controlBackToTop = document.querySelector('.control-back-to-top');
+let header = document.querySelector('.header');
+
+navbar.classList.remove('active');
 
 btnMenu.addEventListener('click', function () {
     navbar.classList.toggle('active');
@@ -10,4 +14,16 @@ closeBtn.addEventListener('click', function () {
 });
 window.onscroll = function () {
     navbar.classList.remove('active');
+    if (window.scrollY > 1000) {
+        controlBackToTop.classList.add('show');
+    } else {
+        controlBackToTop.classList.remove('show');
+    }
 };
+
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth',
+    });
+}
