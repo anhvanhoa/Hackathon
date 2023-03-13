13b7489345768fcd00aad180ec69156ea214f1f2
AnhVanHoa <anhhoccode@gmail.com>
2023-03-13 21:50:49 +0700

    nav

--- js/app.js
+++ js/app.js
@@ -12,6 +12,8 @@ btnMenu.addEventListener('click', function () {
 closeBtn.addEventListener('click', function () {
     navbar.classList.remove('active');
 });
+
+// Back to top
 window.onscroll = function () {
     navbar.classList.remove('active');
     if (window.scrollY > 1000) {
@@ -27,3 +29,44 @@ function scrollToTop() {
         behavior: 'smooth',
     });
 }
+
+// Active navbar item on the first loadding
+window.addEventListener('load', activeMenu);
+
+// Logo click
+const logo = document.querySelector('.header-logo');
+logo.addEventListener('click', () => {
+    window.scrollTo(0, 0);
+});
+
+// Scroll spy
+const items = document.querySelectorAll('.header-item');
+items.forEach((ele) => {
+    ele.addEventListener('click', () => {
+        items.forEach((ele) => {
+            ele.style.setProperty('--background-beforeElement', '0');
+        });
+        ele.style.setProperty('--background-beforeElement', '100%');
+    });
+});
+
+const highValueProduct = document.getElementById('moveProduct');
+const partner = document.getElementById('movePartner');
+const review = document.getElementById('moveReview');
+const benefits = document.getElementById('moveBenefits');
+const contact = document.getElementById('moveContact');
+const partOfWeb = [highValueProduct, partner, review, benefits, contact];
+
+function activeMenu() {
+    partOfWeb.forEach((ele, index) => {
+        if (
+            window.scrollY + header.offsetHeight >= ele.offsetTop &&
+            window.scrollY + header.offsetHeight <= ele.offsetTop + ele.offsetHeight
+        ) {
+            items[index].style.setProperty('--background-beforeElement', '100%');
+        } else {
+            items[index].style.setProperty('--background-beforeElement', '0');
+        }
+    });
+}
+window.addEventListener('scroll', activeMenu);
